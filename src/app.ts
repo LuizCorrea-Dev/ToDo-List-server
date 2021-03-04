e1fa6364bb40ce2e543f834c53ae2cc842f92e6f
LuizCorrea-Dev <correa.luizbatista@gmail.com>
2021-03-04 16:48:54 -0300

    correção

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,8 @@ const app: Express = express()
 
 
 const PORT: string | number = process.env.PORT || 4000
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 app.use(todoRoutes)
 
 const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.vxjn8.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
